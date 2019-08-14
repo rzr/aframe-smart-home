cafc88ac5687ce1ce59ea21570792b4ac92d46c5
Philippe Coval <p.coval@samsung.com>
2019-08-14 12:06:18 +0200

    iotjs: Add index.js for package
    Change-Id: Ia3a71c2e7afea93725f471377fe64ca50dc79871
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ index.js
@@ -0,0 +1,12 @@
+// -*- mode: js; js-indent-level:2;  -*-
+// SPDX-License-Identifier: MPL-2.0
+/**
+ *
+ * Copyright 2018-present Samsung Electronics France SAS, and other contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/#
+ */
+
+module.exports = require('./lib/index');
