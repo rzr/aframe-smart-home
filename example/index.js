f2712d4072f73e1df780c21294c098c95e76e1bd
Philippe Coval <p.coval@samsung.com>
2019-08-14 12:06:56 +0200

    example: Add webthing application
    Change-Id: Ie5443f0aacd27b285433a5d235d26b8bb03d62f5
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ example/index.js
@@ -0,0 +1,18 @@
+// -*- mode: js; js-indent-level:2;  -*-
+// SPDX-License-Identifier: MPL-2.0
+/**
+ *
+ * Copyright 2018-present Samsung Electronics France SAS, and other contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/#
+ */
+
+var app;
+try {
+  app = require('aframe-smart-home');
+} catch (err) {
+  app = require('../lib/index');
+}
+app.start();
