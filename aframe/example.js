74db99f1f69dabee274cf1d1ff13d7d09e27d2bc
Philippe Coval <p.coval@samsung.com>
2019-08-14 12:21:20 +0200

    aframe: Serve for developement
    Change-Id: I8b1fd1c4e1e49e18a2abdb3ee239989bdedc8917
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ aframe/example.js
@@ -0,0 +1,17 @@
+// -*- mode: js; js-indent-level:2;  -*-
+// SPDX-License-Identifier: MPL-2.0
+
+/**
+ *
+ * Copyright 2019-present Samsung Electronics France SAS, and other contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/#
+ */
+const express = require('express');
+const app = express();
+app.use(express.static('.'));
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Listening to:\nhttp://localhost:' + listener.address().port);
+});
